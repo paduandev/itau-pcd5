82b5556ce28666a9faafeb1e768701f21217604b
EmersonPaduan <emerson.paduan@gmail.com>
2021-12-02 13:38:46 -0300

    aula 04 parte 2

---
+++ aula04/exemplos/Exemplo03.java
@@ -0,0 +1,15 @@
+package exemplos;
+
+public class Exemplo03 {
+    public static void main(String[] args) {
+        int n1, n2;
+
+        n1 = 10;
+        n2 = 15;
+
+        System.out.println( n1 < n2 );
+        System.out.println( n1 > n2 );
+        System.out.println( n1 != n2 );
+        System.out.println( n1 == n2 );
+    }
+}
