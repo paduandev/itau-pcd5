d2a98b496d3d5d0ac89bf0041ae7fe1a2bb2ff7c
EmersonPaduan <emerson.paduan@gmail.com>
2021-12-02 09:45:47 -0300

    aula 03 - exercicio 4

--- aula03/exercicios/Exercicio04.java
+++ aula03/exercicios/Exercicio04.java
@@ -5,16 +5,25 @@ import java.util.Scanner;
 public class Exercicio04 {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
+        final double TAXA_POR_KW = 500; // constante = o valor não pode ser alterado
         double salarioMinimo, kwConsumido;
         double valorKW, valorTotal, valorComDesconto;
 
         // entrada
+        System.out.println("Digite o valor do salário mínimo:");
+        salarioMinimo = entrada.nextDouble();
+        System.out.println("Digite o consumo em KW:");
+        kwConsumido = entrada.nextDouble();
 
         // processamento
-        valorKW = salarioMinimo / 500;
+        valorKW = salarioMinimo / TAXA_POR_KW;
+        valorTotal = valorKW * kwConsumido;
+        valorComDesconto = valorTotal * 0.85; // valorTotal - valorTotal * 0.15;
 
         // saída
-
+        System.out.println("1 Kw custa: R$ " + valorKW);
+        System.out.println("Valor Total: R$ " + valorTotal);
+        System.out.println("Valor com 15% de desconto: R$ " + valorComDesconto);
 
         entrada.close();
     }
